33bd065d40af8a7cf13850f3e4dc08cc0a8c95c7
ggikko <ggikko@hotmail.com>
2016-11-06 13:04:26 +0900

    ?

--- CustomDialogPraticeApp/app/src/main/java/ggikko/me/customdialogpraticeapp/MainActivity.java
+++ CustomDialogPraticeApp/app/src/main/java/ggikko/me/customdialogpraticeapp/MainActivity.java
@@ -7,10 +7,12 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
     }
+
 }
--- CustomDialogPraticeApp/app/src/main/java/ggikko/me/customdialogpraticeapp/dialog/GgikkoDialog.java
+++ CustomDialogPraticeApp/app/src/main/java/ggikko/me/customdialogpraticeapp/dialog/GgikkoDialog.java
@@ -3,7 +3,9 @@ package ggikko.me.customdialogpraticeapp.dialog;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,30 +22,43 @@ public class GgikkoDialog extends BaseDialog {
     private NegativeButtonListener negativeButtonListener;
     private String title;
     private String subTitle;
-    private String centerContent;
+    private String content;
+    private String boxContent;
     private String description;
     private String positiveText;
     private String negativeText;
     private int subTitleTextSize;
+    private boolean isHtml;
+    private boolean isDivider;
 
     @BindView(R.id.titleTextView) TextView titleTextView;
     @BindView(R.id.subTitleTextView) TextView subTitleTextView;
-    @BindView(R.id.centerContentTextView) TextView centerContentTextView;
+    @BindView(R.id.contentTextView) TextView contentTextView;
+    @BindView(R.id.boxContentTextView) TextView boxContentTextView;
     @BindView(R.id.descriptionTextView) TextView descriptionTextView;
 
     @BindView(R.id.negativeTextView) TextView negativeTextView;
     @BindView(R.id.positiveTextView) TextView positiveTextView;
+
+    @BindView(R.id.dividerView) View dividerView;
+
+    public GgikkoDialog(){
+
+    }
 
     public GgikkoDialog(Builder builder) {
         this.positiveButtonListener = builder.positiveButtonListener;
         this.negativeButtonListener = builder.negativeButtonListener;
         this.title = builder.title;
         this.subTitle = builder.subTitle;
-        this.centerContent = builder.centerContent;
+        this.content = builder.content;
+        this.boxContent = builder.boxContent;
         this.description = builder.description;
         this.positiveText = builder.positiveText;
         this.negativeText = builder.negativeText;
         this.subTitleTextSize = builder.subTitleTextSize;
+        this.isHtml = builder.isHtml;
+        this.isDivider = builder.isDivider;
     }
 
     @OnClick(R.id.positiveTextView)
@@ -76,18 +91,29 @@ public class GgikkoDialog extends BaseDialog {
 
         if (isStringEmpty(title)) titleTextView.setVisibility(View.GONE);
         if (isStringEmpty(subTitle)) subTitleTextView.setVisibility(View.GONE);
+        if (isStringEmpty(content)) contentTextView.setVisibility(View.GONE);
         if (isStringEmpty(description)) descriptionTextView.setVisibility(View.GONE);
-        if (isStringEmpty(centerContent)) centerContentTextView.setVisibility(View.GONE);
+        if (isStringEmpty(boxContent)) boxContentTextView.setVisibility(View.GONE);
         if (isStringEmpty(positiveText)) positiveTextView.setVisibility(View.GONE);
         if (isStringEmpty(negativeText)) negativeTextView.setVisibility(View.GONE);
+        if (isDivider) dividerView.setVisibility(View.VISIBLE);
 
         titleTextView.setText(title);
-        subTitleTextView.setText(subTitle);
+        contentTextView.setText(content);
         descriptionTextView.setText(description);
-        subTitleTextView.setTextSize(subTitleTextSize);
-        centerContentTextView.setText(centerContent);
+        boxContentTextView.setText(boxContent);
         positiveTextView.setText(positiveText);
         negativeTextView.setText(negativeText);
+
+        if(isHtml){
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                subTitleTextView.setText(Html.fromHtml(subTitle, Html.FROM_HTML_MODE_LEGACY));
+            } else {
+                subTitleTextView.setText(Html.fromHtml(subTitle));
+            }
+        }else{
+            subTitleTextView.setText(subTitle);
+        }
     }
 
     private boolean isStringEmpty(final CharSequence cs) {
@@ -107,11 +133,14 @@ public class GgikkoDialog extends BaseDialog {
         private NegativeButtonListener negativeButtonListener;
         private String title;
         private String subTitle;
-        private String centerContent;
+        private String content;
+        private String boxContent;
         private String description;
         private String positiveText;
         private String negativeText;
         private int subTitleTextSize;
+        private boolean isHtml = false;
+        private boolean isDivider = false;
 
         public Builder(Context context, FragmentManager fragmentManager, String tag) {
             this.context = context;
@@ -123,12 +152,12 @@ public class GgikkoDialog extends BaseDialog {
             return new GgikkoDialog(this);
         }
 
-        public Builder positiveListener(PositiveButtonListener positiveButtonListener) {
+        public Builder onPositive(PositiveButtonListener positiveButtonListener) {
             this.positiveButtonListener = positiveButtonListener;
             return this;
         }
 
-        public Builder negativeListener(NegativeButtonListener negativeButtonListener) {
+        public Builder onNegative(NegativeButtonListener negativeButtonListener) {
             this.negativeButtonListener = negativeButtonListener;
             return this;
         }
@@ -138,14 +167,24 @@ public class GgikkoDialog extends BaseDialog {
             return this;
         }
 
-        public Builder subTitle(String subTitle, int subTitleTextSize) {
+        public Builder subTitle(String subTitle, @Nullable boolean isHtml) {
             this.subTitle = subTitle;
-            this.subTitleTextSize= subTitleTextSize;
+            this.isHtml = isHtml;
             return this;
         }
 
-        public Builder centerContent(String centerContent) {
-            this.centerContent = centerContent;
+        public Builder subTitle(String subTitle) {
+            this.subTitle = subTitle;
+            return this;
+        }
+
+        public Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder boxContent(String boxContent) {
+            this.boxContent = boxContent;
             return this;
         }
 
@@ -161,6 +200,11 @@ public class GgikkoDialog extends BaseDialog {
 
         public Builder negativeText(String negativeText) {
             this.negativeText = negativeText;
+            return this;
+        }
+
+        public Builder useDivider(){
+            isDivider = true;
             return this;
         }
 
